ea6cc24efe3366b6a5bd2bb6652ce2930a93207d
Kartik Singh <kartik.ynwa@gmail.com>
2022-08-30 00:03:22 +0530

    black

--- bmv.py
+++ bmv.py
@@ -11,6 +11,7 @@ fiction_keywords = ("f", "fiction")
 nonfiction_keywords = ("nf", "nonfiction")
 textbook_keywords = ("t", "textbook")
 
+
 def main():
     home = Path.home()
     docsdir = home / "docs"
@@ -19,9 +20,25 @@ def main():
 
     parser = argparse.ArgumentParser(description="Move and rename ePub files")
     parser.add_argument("epub_file", type=str, help="Location of ePub file")
-    parser.add_argument("-t", "--type", type=str, help="Specify genre", default=nonfiction_keywords[0], choices=(fiction_keywords + nonfiction_keywords + textbook_keywords))
-    parser.add_argument("-m", "--move", action="store_true", help="Move the ePub file to the new location (by default only the new filename is printed)")
-    parser.add_argument("--force", action="store_true", help="Overwrite the target file if it already exists. Useless without -m/--move.")
+    parser.add_argument(
+        "-t",
+        "--type",
+        type=str,
+        help="Specify genre",
+        default=nonfiction_keywords[0],
+        choices=(fiction_keywords + nonfiction_keywords + textbook_keywords),
+    )
+    parser.add_argument(
+        "-m",
+        "--move",
+        action="store_true",
+        help="Move the ePub file to the new location (by default only the new filename is printed)",
+    )
+    parser.add_argument(
+        "--force",
+        action="store_true",
+        help="Overwrite the target file if it already exists. Useless without -m/--move.",
+    )
 
     args = parser.parse_args()
 
@@ -65,6 +82,7 @@ def main():
         print("Destination file already exists. Overwriting.")
     shutil.move(args.epub_file, new_path)
     print("File moved :)")
+
 
 if __name__ == "__main__":
     main()
